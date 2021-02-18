e96722910b49057fecfc41e8a223d5b498c5f746
oconda90 <70242240+oconda90@users.noreply.github.com>
2021-02-17 22:50:56 -0800

    added user to models and bug fixes

--- BEW-1.2-Forms-Homework-main/grocery_app/models.py
+++ BEW-1.2-Forms-Homework-main/grocery_app/models.py
@@ -2,6 +2,7 @@ from sqlalchemy_utils import URLType
 
 from grocery_app import db
 from grocery_app.utils import FormEnum
+from flask_login import UserMixin
 
 class ItemCategory(FormEnum):
     """Categories of grocery items."""
@@ -18,6 +19,8 @@ class GroceryStore(db.Model):
     title = db.Column(db.String(80), nullable=False)
     address = db.Column(db.String(200), nullable=False)
     items = db.relationship('GroceryItem', back_populates='store')
+    created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_by = db.relationship('User') 
 
 class GroceryItem(db.Model):
     """Grocery Item model."""
@@ -26,6 +29,23 @@ class GroceryItem(db.Model):
     price = db.Column(db.Float(precision=2), nullable=False)
     category = db.Column(db.Enum(ItemCategory), default=ItemCategory.OTHER)
     photo_url = db.Column(URLType)
-    store_id = db.Column(
-        db.Integer, db.ForeignKey('grocery_store.id'), nullable=False)
+    store_id = db.Column(db.Integer, db.ForeignKey('grocery_store.id'), nullable=False)
     store = db.relationship('GroceryStore', back_populates='items')
+    created_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    created_by = db.relationship('User')
+    shopping_list = db.relationship('User', secondary='shopping_list', back_populates = 'shopping_list_items')
+
+class User(db.Model, UserMixin):
+    """User model."""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    shopping_list_items = db.relationship('GroceryItem', secondary='shopping_list', back_populates = 'shopping_list')
+    
+    def __str__(self):
+        return f'{self.username}'
+
+shopping_list_table = db.Table('shopping_list',
+    db.Column('groceryitem_id', db.Integer, db.ForeignKey('grocery_item.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+)
